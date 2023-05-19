963c24d3f9c8ffefc0dee86e66c107524ed45ab1
rinsola <olimeed@gmail.com>
2023-05-19 14:30:14 +0100

    react starter files

---
+++ client/plant-pal/src/App.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div className='App'>
+      <header className='text-red-500'>test that app works</header>
+    </div>
+  );
+}
+
+export default App;
