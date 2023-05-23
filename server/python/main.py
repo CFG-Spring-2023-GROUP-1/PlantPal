6638ce4ae9cabad570944a0381013d91dc9504f3
Emina Ergul <eminaergul712@gmail.com>
2023-05-23 09:17:14 +0100

    added ask_disease function from user own input + refactor main.py

--- server/python/main.py
+++ server/python/main.py
@@ -15,11 +15,8 @@ load_dotenv()
 user_input_plant = input('Please enter a house plant by common name or latin name,'
                          'or enter "My Plants" to view your plant collection.')
 
-# Haven't started on the remove_plant logic in this file yet
+# Haven't started on the remove_plant or display_my_plants logic in this file yet
 # plant_name = input("Enter the name of a plant to remove it from your collection: ")
-
-if user_input_plant == 'My Plants':
-    get_all_myplants()
 
 host = 'house-plants2.p.rapidapi.com'
 url = f"{base_url}search"
@@ -35,13 +32,8 @@ full_data = response.json()
 plant_id = full_data[0]['refIndex']
 data = full_data[0]['item']
 
-
 def replace_none(stat):
-    if stat is None:
-        return 'N/A'
-    else:
-        return stat
-
+    return stat if stat is not None else 'N/A'
 
 plant_data = {
     'latin_name': data.get('Latin name', 'N/A'),
@@ -56,17 +48,8 @@ plant_data = {
     'image': data.get('Img', 'N/A')
 }
 
-def ask_disease(plant_data):
-    if plant_data['common_diseases'] == 'N/A':
-        return
-    else:
-        user_input_disease = input(f'Does your plant currently have any disease? Choose from: {plant_data["common_diseases"]}')
-        return user_input_disease
-
-ask_disease(plant_data)
-
 def display_from_user_input(plant_data):
-    return print(
+    return (
         f'Latin name: {plant_data["latin_name"]}\n'
         f'Common name(s): {plant_data["common_name"]}\n'
         f'Ideal light: {plant_data["light_level"]}\n'
@@ -79,15 +62,27 @@ def display_from_user_input(plant_data):
         f'Image: {plant_data["image"]}\n'
         )
 
-display_from_user_input(plant_data)
+print(display_from_user_input(plant_data))
 
-def ask_add_plant(data, plant_id):
+def ask_disease(plant_data):
+    if plant_data['common_diseases'] == 'N/A':
+        return None
+    else:
+        common_diseases = plant_data['common_diseases']
+        while True:
+            user_input_disease = input(f'Does your plant currently have any disease? Choose from: {common_diseases}')
+            if user_input_disease in common_diseases:
+                return user_input_disease
+            else:
+                print('Disease entered not found. Please try again.')
+
+
+def ask_add_plant(data, plant_id, user_input_disease=None):
     add_plant_question = input(f'Would you like to add {user_input_plant} to your My plants collection? Please enter "y" for yes, or "n" for no')
     if add_plant_question == 'n':
         print(f'{user_input_plant} was not added')
     elif add_plant_question == 'y':
-        add_plant(data, plant_id)
-    return data, plant_id
+        return add_plant(data, plant_id, user_input_disease)
 
 
-ask_add_plant(data, plant_id)
+ask_add_plant(data, plant_id, user_input_disease=ask_disease(plant_data))
