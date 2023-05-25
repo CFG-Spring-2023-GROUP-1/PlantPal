93f8e091be35a9867a70e4fd205885ebbc99fd44
Emina Ergul <eminaergul712@gmail.com>
2023-05-25 14:02:08 +0100

    refactoring after sql PlantDetails table column sync

--- server/python/main.py
+++ server/python/main.py
@@ -14,11 +14,17 @@ load_dotenv()
 
 # remove plant from My Plants that matches select_plant user input
 def remove_from_my_plants(plants):
-    select_plant = input('Enter which plant you want to remove by scientific(latin) name.')
-    chosen_entry = [plant for plant in plants if plant[2] == select_plant] # select scientific name from the correct tuple
-    while select_plant != chosen_entry[0][2]:
+    print(plants)
+    select_plant = input('Enter the scientific(latin) name of the plant you want to remove.')
+    chosen_entry = [plant for plant in plants if plant[2].lower() == select_plant.lower()] # select scientific name from the correct tuple
+    print(chosen_entry[0][2])
+    while not chosen_entry:
         print('Plant not found, please try again.')
-        select_plant = input('Enter which plant you want to remove.')
+        select_plant = input('Enter the scientific (Latin) name of the plant you want to remove: ')
+        chosen_entry = [plant for plant in plants if plant[2] == select_plant]
+    while select_plant.lower() != chosen_entry[0][2].lower():
+        print('Plant not found, please try again.')
+        select_plant = input('Enter the scientific(latin) name of the plant you want to remove.')
     remove_plant(select_plant)
 
 # Show user each plant in their collection
@@ -31,53 +37,17 @@ def display_my_plants():
     elif not plants:
         print('Your plant collection is empty.')
         return
-
-
-
     # ask user if they want to remove any plant from displayed collection
     ask_remove_plant = input('Would you like to remove a plant from your collection? (y/n)')
     if ask_remove_plant.lower() == 'y':
         remove_from_my_plants(plants)
+    elif ask_remove_plant.lower() == 'n':
+        search_plant()
 
-
-user_input_plant = input('Please enter a house plant by common name or latin name,'
-                         'or enter "My Plants" to view your plant collection.')
-
-if user_input_plant.lower() == 'my plants':
-    display_my_plants()
-
-
-host = 'house-plants2.p.rapidapi.com'
-url = f"{base_url}search"
-querystring = {"query": user_input_plant}
-headers = {
-    "X-RapidAPI-Key": api_key,
-    "X-RapidAPI-Host": host
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-full_data = response.json()
-plant_id = full_data[0]['refIndex']
-data = full_data[0]['item']
 
 def replace_none(stat):
     return stat if stat is not None else 'N/A'
 
-plant_data = {
-    'latin_name': data.get('Latin name', 'N/A'),
-    'common_name': ', '.join(data.get('Common name', [])),
-    'light_level': replace_none(data.get('Light tolered')),
-    'watering': data.get('Watering', 'N/A'),
-    'climate': data.get('Climat', 'N/A'),
-    'max_temp': data.get('Temperature max', {}),
-    'min_temp': data.get('Temperature min', {}),
-    'growth_speed': data.get('Growth', 'N/A'),
-    'common_diseases': replace_none(data.get("Disease")),
-    'image': data.get('Img', 'N/A')
-}
-
-# show user details of the plant they searched for
 def display_from_user_input(plant_data):
     return (
         f'Latin name: {plant_data["latin_name"]}\n'
@@ -92,8 +62,6 @@ def display_from_user_input(plant_data):
         f'Image: {plant_data["image"]}\n'
         )
 
-print(display_from_user_input(plant_data))
-
 
 # if common_diseases in plant_data is not none, ask for user input on plant disease
 def ask_disease(plant_data):
@@ -102,21 +70,77 @@ def ask_disease(plant_data):
     else:
         common_diseases = plant_data['common_diseases']
         while True:
-            user_input_disease = input(f'Does your plant currently have any disease? Choose from: {common_diseases}')
-            if user_input_disease in common_diseases:
+            user_input_disease = input(f'Does your plant currently have any disease? Choose from: {common_diseases}, or enter "none".')
+            if user_input_disease.lower() == 'none':
+                return False
+            elif user_input_disease.lower() in common_diseases.lower():
                 return user_input_disease
             else:
                 print('Disease entered not found. Please try again.')
 
 
 # ask user if they want to add searched for plant to My Plants
-def ask_add_plant(data, plant_id, user_input_disease=None):
-    add_plant_question = input(f'Would you like to add {user_input_plant} to your My plants collection? Please enter "y" for yes, or "n" for no')
-    if add_plant_question == 'n':
-        print(f'{user_input_plant} was not added')
-    elif add_plant_question == 'y':
-        return add_plant(data, plant_id, user_input_disease)
+def ask_add_plant(data, plant_id, user_input_plant, user_input_disease=None):
+    added = False
+    while not added:
+
+        add_plant_question = input(
+            f'Would you like to add {user_input_plant} to your My plants collection? (y/n)')
+        if add_plant_question.lower() == 'n':
+            print(f'{user_input_plant} was not added')
+            added = True
+        elif add_plant_question.lower() == 'y':
+            added = True
+            return add_plant(data, plant_id, user_input_disease)
 
 
-ask_add_plant(data, plant_id, user_input_disease=ask_disease(plant_data))
+# call the api with user input
+def search_plant():
+    while True:
+        user_input_plant = input('Please enter a house plant by common name or latin name,'
+                                 'or enter "My Plants" to view your plant collection.')
+
+        if user_input_plant.lower() == 'my plants':
+            display_my_plants()
+
+
+        host = 'house-plants2.p.rapidapi.com'
+        url = f"{base_url}search"
+        querystring = {"query": user_input_plant}
+        headers = {
+            "X-RapidAPI-Key": api_key,
+            "X-RapidAPI-Host": host}
+        # Make the API request
+        response = requests.get(url, headers=headers, params=querystring)
+
+        if response.status_code == 200:
+            full_data = response.json()
+            if full_data:
+                plant_id = full_data[0]['refIndex']
+                data = full_data[0]['item']
+                if data:
+                    plant_data = {
+                        'latin_name': data.get('Latin name', 'N/A'),
+                        'common_name': ', '.join(data.get('Common name', [])),
+                        'light_level': replace_none(data.get('Light tolered')),
+                        'watering': data.get('Watering', 'N/A'),
+                        'climate': data.get('Climat', 'N/A'),
+                        'max_temp': data.get('Temperature max', {}),
+                        'min_temp': data.get('Temperature min', {}),
+                        'growth_speed': data.get('Growth', 'N/A'),
+                        'common_diseases': replace_none(data.get("Disease")),
+                        'image': data.get('Img', 'N/A')
+                    }
+                    print(display_from_user_input(plant_data))
+                    ask_add_plant(data, plant_id, user_input_plant, user_input_disease=ask_disease(plant_data))
+                    # add_plant(data, plant_id, user_input_disease=ask_disease(plant_data))
+                else:
+                    print("No plant found. Please enter a valid plant type.")
+            else:
+                print("Error: Failed to fetch plant data. Please try again.")
+
+
+search_plant()
+
+
 
