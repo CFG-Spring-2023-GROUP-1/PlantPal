3040dc6fa5cad2334c7d0a75678bbf2b87a5a63e
rinsola <olimeed@gmail.com>
2023-05-28 10:58:31 +0100

    text-fix

--- server/python/tests/test_user.py
+++ server/python/tests/test_user.py
@@ -89,12 +89,15 @@ class TestUser(TestCase):
     def test_no_users_exist(self, mock_users):
         mock_users.return_value = []
         result = User.get_users()
-        self.assertTrue(len(result) is 0)
+        self.assertTrue(len(result) == 0)
 
-    def test_get_user_id(self):
-        self.assertEqual(user_one.get_user_id(), u_id)
-        self.assertTrue(user_one.get_user_id())
-        self.assertFalse(not user_one.get_user_id())
+    @mock.patch("user.User.get_user_id")
+    def test_get_user_id(self, mock_user_id):
+        mock_user_id.return_value = u_id
+        result = user_one.get_user_id()
+        self.assertEqual(result, u_id)
+        self.assertTrue(result is not None)
+        self.assertFalse(result is None)
 
     @mock.patch("user.User.get_user_id")
     def test_no_user_id(self, mock_id):
