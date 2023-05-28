4882ef71d8f843a140c4677d2561731603ce65d9
rinsola <olimeed@gmail.com>
2023-05-28 10:53:51 +0100

    text-fix

--- server/python/tests/test_user.py
+++ server/python/tests/test_user.py
@@ -2,13 +2,22 @@ import unittest
 from unittest import TestCase, mock
 from unittest.mock import patch
 import sys
-sys.path.append('../')
+import datetime
+
+sys.path.append("../")
 from user import User
 
-data = {'1990-01-01', '+1234567890', 'password123', 'emina.ergul@example.com',
-        '123 Main Street, City, Country', 'Ergul', 'Emina'}
+data = {
+    "1990-01-01",
+    "+1234567890",
+    "password123",
+    "emina.ergul@example.com",
+    "123 Main Street, City, Country",
+    "Ergul",
+    "Emina",
+}
 
-u_id = '6ea633e3-87f8-4807-bd16-2bdba0a8869b'
+u_id = "6ea633e3-87f8-4807-bd16-2bdba0a8869b"
 
 login1 = {
     "Email": "emina.ergul@example.com",
@@ -39,14 +48,30 @@ register_data = {
     "Address": "123 Leow Street, City, Country",
     "Password": "password123",
 }
-output = {'FirstName': 'Joanne',
-          'LastName': 'Leow',
-          'Email': 'feranmi.ayo@example.com',
-          'PhoneNo': '+3234567890',
-          'Dob': '1993-01-01',
-          'Address': '123 Leow Street, City, Country',
-          'Password': 'password123', 'status': 'success',
-          'message': 'user added successfully'}
+output = {
+    "FirstName": "Joanne",
+    "LastName": "Leow",
+    "Email": "feranmi.ayo@example.com",
+    "PhoneNo": "+3234567890",
+    "Dob": "1993-01-01",
+    "Address": "123 Leow Street, City, Country",
+    "Password": "password123",
+    "status": "success",
+    "message": "user added successfully",
+}
+
+users = [
+    {
+        "Address": "123 Leow Street, City, Country",
+        "Dob": datetime.date(1993, 1, 1),
+        "Email": "feranmi.ayo@example.com",
+        "FirstName": "Joanne",
+        "LastName": "Leow",
+        "Password": "$2b$12$4IiNAiLp.rs0CeP7eJhuL.NQj0O7.R17riyO.6NAD/hdn/KIfGb7q",
+        "PhoneNo": "+3234567890",
+        "UserId": "0c92f4af-234e-4437-800c-38496d0f0dc6",
+    }
+]
 
 
 class TestUser(TestCase):
@@ -54,53 +79,55 @@ class TestUser(TestCase):
         self.assertEqual(user_one.get_data(), data)
         self.assertTrue(user_one.get_data())
 
-    @staticmethod
-    def test_users_exist():
-        users = User.get_users()
-        assert len(users) > 0
+    @mock.patch("user.User.get_users")
+    def test_users_exist(self, mock_users):
+        mock_users.return_value = users
+        result = User.get_users()
+        assert len(result) > 0
 
-    def test_no_users_exist(self):
-        users = User.get_users()
-        self.assertTrue(len(users))
+    @mock.patch("user.User.get_users")
+    def test_no_users_exist(self, mock_users):
+        mock_users.return_value = []
+        result = User.get_users()
+        self.assertTrue(len(result) is 0)
 
     def test_get_user_id(self):
         self.assertEqual(user_one.get_user_id(), u_id)
         self.assertTrue(user_one.get_user_id())
         self.assertFalse(not user_one.get_user_id())
 
-    @mock.patch('user.User.get_user_id')
+    @mock.patch("user.User.get_user_id")
     def test_no_user_id(self, mock_id):
         mock_id.return_value = None
         result = user_one.get_user_id()
         self.assertFalse(result)
 
-    @mock.patch('user.User.login')
+    @mock.patch("user.User.login")
     def test_login(self, mock_id):
         mock_id.return_value = user_one
         result = user_one.login(login1)
         self.assertTrue(result)
 
-    @mock.patch('user.User.login')
+    @mock.patch("user.User.login")
     def test_incorrect_login(self, mock_id):
-        mock_id.return_value = 'Incorrect Username or Password'
+        mock_id.return_value = "Incorrect Username or Password"
         result = user_one.login(login2)
-        self.assertEqual(result, 'Incorrect Username or Password')
+        self.assertEqual(result, "Incorrect Username or Password")
 
-    @mock.patch('user.User.verify_password')
+    @mock.patch("user.User.verify_password")
     def test_verify_password(self, mock_password):
-        mock_password.return_value = 'password123'
+        mock_password.return_value = "password123"
         result = user_one.verify_password()
-        self.assertEqual(result, 'password123')
+        self.assertEqual(result, "password123")
 
-    @mock.patch('user.User.verify_password')
+    @mock.patch("user.User.verify_password")
     def test_wrong_password(self, mock_password):
-        mock_password.return_value = 'password123'
+        mock_password.return_value = "password123"
         result = user_one.verify_password()
-        self.assertIsNot(result, 'password124')
+        self.assertIsNot(result, "password124")
 
-    @mock.patch('user.User.add_user')
+    @mock.patch("user.User.add_user")
     def test_add_user(self, mock_password):
         mock_password.return_value = output
         result = user_one.add_user(register_data)
         self.assertEqual(result, output)
-
